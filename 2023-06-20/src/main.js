4db40cb733a3f994a149d47537b580e343718471
murat <merdo.net@gmail.com>
2023-06-26 18:57:52 +0200

    mdi icons

--- 2023-06-20/src/main.js
+++ 2023-06-20/src/main.js
@@ -8,6 +8,9 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css";
+
 const vuetify = createVuetify({
   components,
   directives,
@@ -15,7 +18,9 @@ const vuetify = createVuetify({
     defaultSet: "mdi",
     aliases,
     sets: {
-      mdi,
+      sets: {
+        mdi,
+      },
     },
   },
 });
